9bdf5ceddd111197e7c63f715ed46433d7e6f465
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2025-05-27 14:58:16 +0530

    Update Revanced-Experiment.sh

--- src/build/Revanced-Experiment.sh
+++ src/build/Revanced-Experiment.sh
@@ -8,20 +8,8 @@ source src/build/utils.sh
 dl_gh "ReVancedExperiments" "Aunali321" "latest"
 dl_gh "revanced-cli" "revanced" "latest"
 
-#################################################
-
-# Patch Telegram CH Play Version:
-get_patches_key "telegram-revanced-experiments"
-get_apk "org.telegram.messenger" "telegram" "telegram" "telegram-fz-llc/telegram/telegram" "arm64-v8a" "nodpi"
-patch "telegram" "revanced-experiments"
-
-# Patch Telegram Web Version:
-get_patches_key "telegram-revanced-experiments"
-get_apk "org.telegram.messenger" "telegram-web-version" "telegram-web-version" "telegram-fz-llc/telegram-web-version/telegram-web-version"
-patch "telegram-web-version" "revanced-experiments"
-
 # Patch Instagram:
 get_patches_key "instagram-revanced-experiments"
 get_apk "com.instagram.android" "instagram-arm64-v8a" "instagram-instagram" "instagram/instagram-instagram/instagram" "arm64-v8a" "nodpi"
-version="383.0.0.0.47"
+version="381.2.0.53.84"
 patch "instagram-arm64-v8a" "revanced-experiments"
