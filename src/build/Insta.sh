a4e0ccec41e662893b073547cf4d86764e06a928
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2025-06-17 13:17:41 +0530

    Create Insta.sh

---
+++ src/build/Insta.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Revanced build
+source ./src/build/utils.sh
+# Download requirements
+revanced_dl(){
+	dl_gh "revanced-patches revanced-cli" "revanced" "latest"
+}
+1() {
+	revanced_dl
