51e70f7862f11d4477dbc04b8b573464489de285
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2025-06-19 17:30:46 +0530

    Update Revanced.sh

--- src/build/Revanced.sh
+++ src/build/Revanced.sh
@@ -14,9 +14,21 @@ revanced_dl(){
 	# Patch Instagram:
 	# Arm64-v8a
 	get_patches_key "instagram"
-	url="https://instagram.en.uptodown.com/android/download/1079737991" #Use uptodown because apkmirror ban tiktok in US lead github action can't download apk file
-	url="https://dw.uptodown.com/dwn/$(req "$url" - | $pup -p --charset utf-8 'button#detail-download-button attr{data-url}')"	
- 	req "$url" "instagram-arm64-v8a.apk"
+page_url="https://instagram.en.uptodown.com/android/download/1079737991"
+
+# Define 'req' function to download files
+req() {
+  curl -L -o "$2" "$1"
+}
+
+# Fetch the actual data-url using pup (ensure 'pup' is installed)
+download_path=$(curl -sL "$page_url" | pup 'button#detail-download-button attr{data-url}')
+
+# Compose the real APK download URL
+apk_url="https://dw.uptodown.com/dwn/$download_path"
+
+# Download the APK
+req "$apk_url" "instagram-arm64-v8a.apk"
 	patch "instagram-arm64-v8a" "ReVancedExperiments" "Aunali321"
 }
 12() {
