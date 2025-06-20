6ce4743c1be9b5862044e3fd67521a037ae83c2f
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2025-06-20 12:19:13 +0530

    Update Revanced-Experiment.sh

--- src/build/Revanced-Experiment.sh
+++ src/build/Revanced-Experiment.sh
@@ -12,6 +12,7 @@ dl_gh "revanced-cli" "revanced" "latest"
 
 # Patch Telegram CH Play Version:
 # Patch Instagram:
-  get_patches_key "instagram"
+  	get_patches_key "instagram"
+   	version="379.1.0.43.80"
 	get_apk "com.instagram.android" "instagram-arm64-v8a" "instagram-instagram" "instagram/instagram-instagram/instagram" "arm64-v8a" "nodpi"
 	patch "instagram-arm64-v8a" "revanced"
