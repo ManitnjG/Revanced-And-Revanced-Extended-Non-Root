37566e99916a2f7c7f3dfb323cf845e05acca5d1
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2025-06-20 11:21:38 +0530

    Update Revanced-Experiment.sh

--- src/build/Revanced-Experiment.sh
+++ src/build/Revanced-Experiment.sh
@@ -13,5 +13,19 @@ dl_gh "revanced-cli" "revanced" "latest"
 # Patch Telegram CH Play Version:
 # Patch Instagram:
 get_patches_key "instagram-revanced-experiments"
-get_apk "com.instagram.android" "instagram-arm64-v8a" "instagram/instagram-instagram" "instagram" "640dpi"
+get_apk() {
+  PACKAGE=$1
+  OUTPUT_NAME=$2
+  MIRROR_PATH=$3
+  APP_NAME=$4
+  DPI=$5
+
+  URL="https://www.apkmirror.com/apk/${MIRROR_PATH}/${APP_NAME}-378-0-0-52-68-release/"
+  
+  # Download matching APK
+  get-apk \
+    --url "$URL" \
+    --pattern "378.0.0.52.68.*arm64-v8a.*${DPI}.*\\.apk$" \
+    --output "${OUTPUT_NAME}.apk"
+}
 patch "instagram-arm64-v8a" "revanced-experiments"
