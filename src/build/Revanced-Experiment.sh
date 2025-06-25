c5540b614b3dd58255084953c06538d12e071b7d
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2025-06-25 17:11:54 +0530

    Update Revanced-Experiment.sh

--- src/build/Revanced-Experiment.sh
+++ src/build/Revanced-Experiment.sh
@@ -11,7 +11,7 @@ dl_gh "revanced-cli" "revanced" "latest"
 #################################################
   	get_patches_key "instagram"
    	wget https://apks.39b7cb94d40914bac590886981b0ed6e.r2.cloudflarestorage.com/com.instagram.android/386.0.0.19.84/379005606.7f8984c2beb5ae09aeac17b80d77173d3ed12ffe.apk?response-content-disposition=attachment%3B%20filename%3D%22Instagram_386.0.0.19.84_apkcombo.com.apk%22&response-content-type=application%2Fvnd.android.package-archive&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20250625T105809Z&X-Amz-SignedHeaders=host&X-Amz-Expires=14400&X-Amz-Credential=3cb727b4cd4780c410b780ac7caa4da3%2F20250625%2Fauto%2Fs3%2Faws4_request&X-Amz-Signature=5bd5dad828af78cb3b3d5336c83253be82d608a76084f22dfe5486af5dd24b70
-	patch "instagram-arm64-v8a" "revanced" "instagram"
+	patch "Instagram_386.0.0.19.84_apkcombo.com" "revanced" "instagram"
 
 dl_gh "revanced-patches" "anddea" "latest"
 
