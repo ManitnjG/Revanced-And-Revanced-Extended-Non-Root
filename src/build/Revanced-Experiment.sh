fa5b110bcf57a9b3464a56a4d483195a573dc158
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2025-06-27 12:15:39 +0530

    Update Revanced-Experiment.sh

--- src/build/Revanced-Experiment.sh
+++ src/build/Revanced-Experiment.sh
@@ -10,8 +10,8 @@ dl_gh "revanced-cli" "revanced" "latest"
 
 #################################################
   	get_patches_key "instagram"
-   	wget https://apks.39b7cb94d40914bac590886981b0ed6e.r2.cloudflarestorage.com/com.instagram.android/386.0.0.19.84/379005606.7f8984c2beb5ae09aeac17b80d77173d3ed12ffe.apk?response-content-disposition=attachment%3B%20filename%3D%22Instagram_386.0.0.19.84_apkcombo.com.apk%22&response-content-type=application%2Fvnd.android.package-archive&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20250625T105809Z&X-Amz-SignedHeaders=host&X-Amz-Expires=14400&X-Amz-Credential=3cb727b4cd4780c410b780ac7caa4da3%2F20250625%2Fauto%2Fs3%2Faws4_request&X-Amz-Signature=5bd5dad828af78cb3b3d5336c83253be82d608a76084f22dfe5486af5dd24b70
-	patch "Instagram_386.0.0.19.84_apkcombo.com" "revanced" "instagram"
+	wget "https://apkcombo.com/r2?u=https%3A%2F%2Fapks.39b7cb94d40914bac590886981b0ed6e.r2.cloudflarestorage.com%2Fcom.instagram.android%2F387.0.0.0.61%2F379103159.538c187a90cb4972779ad204d6c679db01483eda.apk%3Fresponse-content-disposition%3Dattachment%253B%2520filename%253D%2522Instagram_387.0.0.0.61_apkcombo.com.apk%2522%26response-content-type%3Dapplication%252Fvnd.android.package-archive%26X-Amz-Algorithm%3DAWS4-HMAC-SHA256%26X-Amz-Date%3D20250627T062325Z%26X-Amz-SignedHeaders%3Dhost%26X-Amz-Expires%3D14400%26X-Amz-Credential%3D3cb727b4cd4780c410b780ac7caa4da3%252F20250627%252Fauto%252Fs3%252Faws4_request%26X-Amz-Signature%3D4e3d8eff59856b3452d13d54d124528c3519ca22964b802d9b58f26254b2caa2&fp=e132b61254764eedfadc8468c86d3029&ip=61.246.213.190&package_name=com.instagram.android&lang=en" -O Instagram_387.0.0.0.61.apk
+	patch "instagram-arm64-v8a" "revanced" "instagram"
 
 dl_gh "revanced-patches" "anddea" "latest"
 
