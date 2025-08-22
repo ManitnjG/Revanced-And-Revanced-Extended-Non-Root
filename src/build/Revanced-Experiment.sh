236e42766210156541c3e85f9103db0303c99c6c
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2025-08-22 16:14:54 +0530

    Update Revanced-Experiment.sh

--- src/build/Revanced-Experiment.sh
+++ src/build/Revanced-Experiment.sh
@@ -4,7 +4,8 @@ source src/build/utils.sh
 
 dl_gh "revanced-patches" "anddea" "latest"
 dl_gh "revanced-cli" "inotia00" "latest"
-get_apk "com.google.android.youtube" "youtube-stable" "youtube" "google-inc/youtube/youtube" "Version=20.33.38" "Bundle_extract" 
+version"20.33.38"
+get_apkpure "com.google.android.youtube" "youtube-stable" "youtube/com.google.android.youtube"
 get_patches_key "youtube-rve-anddea"
 patch "youtube-stable" "anddea" "inotia"
 
