3cba0271a644d61759cf94f52ebae1cc79c2451f
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2025-08-22 10:54:57 +0530

    Update Revanced-Experiment.sh

--- src/build/Revanced-Experiment.sh
+++ src/build/Revanced-Experiment.sh
@@ -5,23 +5,33 @@ source src/build/utils.sh
 #################################################
 
 # Download requirements
-dl_gh "ReVancedExperiments" "Aunali321" "latest"
-dl_gh "revanced-cli" "revanced" "latest"
+dl_gh "revanced-patches" "anddea" "latest"
+dl_gh "revanced-cli" "inotia00" "latest"
+
+# Patch YouTube:
+get_patches_key "youtube-rve-anddea"
+get_apk "com.google.android.youtube" "youtube-stable" "youtube" "google-inc/youtube/youtube" "Bundle_extract"
+version="20.33.38"
+split_editor "youtube-stable" "youtube-stable"
+patch "youtube-stable" "anddea" "inotia"
+
+#dl_gh "ReVancedExperiments" "Aunali321" "latest"
+#dl_gh "revanced-cli" "revanced" "latest"
 
 #################################################
 
 # Patch Telegram CH Play Version:
-get_patches_key "telegram-revanced-experiments"
-get_apk "org.telegram.messenger" "telegram" "telegram" "telegram-fz-llc/telegram/telegram" "arm64-v8a" "nodpi"
-patch "telegram" "revanced-experiments"
+#get_patches_key "telegram-revanced-experiments"
+#get_apk "org.telegram.messenger" "telegram" "telegram" "telegram-fz-llc/telegram/telegram" "arm64-v8a" "nodpi"
+#patch "telegram" "revanced-experiments"
 
 # Patch Telegram Web Version:
-get_patches_key "telegram-revanced-experiments"
-get_apk "org.telegram.messenger" "telegram-web-version" "telegram-web-version" "telegram-fz-llc/telegram-web-version/telegram-web-version"
-patch "telegram-web-version" "revanced-experiments"
+#get_patches_key "telegram-revanced-experiments"
+#get_apk "org.telegram.messenger" "telegram-web-version" "telegram-web-version" "telegram-fz-llc/telegram-web-version/telegram-web-version"
+#patch "telegram-web-version" "revanced-experiments"
 
 # Patch Instagram:
-get_patches_key "instagram-revanced-experiments"
-version="362.0.0.33.241"
-get_apk "com.instagram.android" "instagram-arm64-v8a" "instagram-instagram" "instagram/instagram-instagram/instagram-instagram" "arm64-v8a" "nodpi"
-patch "instagram-arm64-v8a" "revanced-experiments"
+#get_patches_key "instagram-revanced-experiments"
+#version="362.0.0.33.241"
+#get_apk "com.instagram.android" "instagram-arm64-v8a" "instagram-instagram" "instagram/instagram-instagram/instagram-instagram" "arm64-v8a" "nodpi"
+#patch "instagram-arm64-v8a" "revanced-experiments"
